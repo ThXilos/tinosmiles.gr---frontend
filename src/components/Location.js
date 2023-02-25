daedf34670321cddbce48564a08b1c721e8379ff
ThXilos <theodksylo@gmail.com>
2023-02-25 10:06:04 +0200

    add google map section

--- src/components/Location.js
+++ src/components/Location.js
@@ -1,6 +1,16 @@
 import React from "react";
 const Location = () => {
-  return <h2>Location Snippet</h2>;
+  return (
+    <div className="container">
+      <iframe
+        title="tinosmiles-custom-map"
+        src="https://snazzymaps.com/embed/234638"
+        width="100%"
+        height="500px"
+        style={{ border: "none", display: "flex" }}
+      ></iframe>
+    </div>
+  );
 };
 
 export default Location;
