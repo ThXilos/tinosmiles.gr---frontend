303302d07a6ab37cf784688b7790f48f69dfe985
ThXilos <theodksylo@gmail.com>
2024-05-08 11:27:30 +0300

    Gas station update

--- src/utils/stations.js
+++ src/utils/stations.js
@@ -4,14 +4,14 @@ export const gasStationInfo = [
     locationName: "Isternia",
     hours: "",
     location: "https://goo.gl/maps/g9nkrKn2TDXDzjZw5",
-    active: true,
+    active: false,
   },
   {
     company: "Aegean",
     locationName: "Tzados",
     hours: "",
     location: "https://goo.gl/maps/5gAGuLecqdVkvrZ99",
-    active: true,
+    active: false,
   },
   {
     company: "Elin",
@@ -22,13 +22,14 @@ export const gasStationInfo = [
   },
   {
     company: "Shell",
-    locationName: "Tripotamos",
+    locationName: "Town",
     hours: "",
     location: "https://goo.gl/maps/Z5tDMFFJBwiFsW3h8",
-    active: false,
+    active: true,
   },
   {
     company: "Shell",
+    locationName: "Town",
     hours: "",
     location: "https://goo.gl/maps/S8WXY8Joi6bmKDeR7",
     active: false,
@@ -38,6 +39,6 @@ export const gasStationInfo = [
     locationName: "Town",
     hours: "",
     location: "https://goo.gl/maps/XjmdoVWpgS3Y6xEa7",
-    active: true,
+    active: false,
   },
 ];
